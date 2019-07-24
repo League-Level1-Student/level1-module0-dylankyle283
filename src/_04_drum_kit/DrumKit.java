f2ac758132f6f46e8fa3343671f199480d17e7c5
League Student <league@iMac-10.attlocal.net>
2019-07-24 16:28:52 -0700

    i did drum kit and almost finished bodypartquiz

--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -21,7 +21,7 @@ import javax.swing.JPanel;
 public class DrumKit implements MouseListener {
 
 	JLabel drumLabelWithImage;
-
+JLabel cymbalLabelWithImage;
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
@@ -57,15 +57,23 @@ frame.pack();
 drumLabelWithImage.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
+String cymbal = "cymbal.jpg" ;
 
+
+cymbalLabelWithImage = createLabelImage(cymbal);
+	panel.add(cymbalLabelWithImage);
+	frame.pack();
+	cymbalLabelWithImage.addMouseListener(this);
+	
+	
 	}
-
+	
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch
 		// the console to see when this is printed.
 System.out.println("mouse clicked");
 		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
-														// that the mouse
+			System.out.println(drumClicked);											// that the mouse
 														// clicked on
 
 		// 15. Download a drum sound and drop it into your "default package".
@@ -74,7 +82,15 @@ System.out.println("mouse clicked");
 
 		// 16. If they clicked on the drumImage...
 
-		// 17. ...use the playSound method to play a drum sound. Test to see if
+		if (drumLabelWithImage == drumClicked) {
+			playSound("drum.wav");
+		}
+    if (cymbalLabelWithImage == drumClicked) {
+		playSound("cymbal.wav");
+	}
+	
+
+// 17. ...use the playSound method to play a drum sound. Test to see if
 		// it works
 
 	}
--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -39,37 +39,44 @@ public class BodyPartQuiz {
 		imageIterator = imageList.iterator();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.add(panel);
-
+window.setSize(0, 0);
 		// 3. Change the size of the window so that you can only see part of the
 		// image.
-		window.setSize(500, 500);
-
-		showNextImage();
-
+	window.setTitle("test");
+window.setSize(500, 500);
+window.setVisible(true);
+	showNextImage();
 	}
 
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+int score = 0;
 		// 2. Set the size of the window in the initializeGui() method 
+for (int i = 0; i < 4; i++) {
+	
 
 		// 4. Ask the user who this person is and store their answer
 		String guess = JOptionPane.showInputDialog("who is this?");
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+if (guess.equals(getNextImage())) {
+	System.out.println("you are right");
+score ++ ;
+}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-
+else {
+	System.out.println("you are wrong");
+}
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
-
+}
 		// 8. .... repeat 4-7 for all your images.....
 
 		// 9. Show them their current score
-
+System.out.println(score);
 	}
 
 	public void showNextImage() {
