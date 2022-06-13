aab99c3a48feb91dd143195e6a9fccd2467ce520
root <root@d9b9c364f57b>
2022-06-13 21:00:11 +0000

    i did houses

--- src/_01_houses/houses.java
+++ src/_01_houses/houses.java
@@ -1,5 +1,6 @@
 package _01_houses;
 
+import java.awt.Color;
 import java.lang.reflect.Method;
 
 import javax.swing.JOptionPane;
@@ -21,23 +22,70 @@ drawHouse();
 	
  static void drawHouse(){
 		Robot rob = new Robot("mini");
-		String househeight = JOptionPane.showInputDialog("how tall do you want the houses");
-		int height = Integer.parseInt(househeight);
+		
+		
 		rob.setX(50);
 		rob.setY(500);
 		rob.setSpeed(100);
 
 	 for (int i = 0; i < 10; i++) {
-					rob.penDown();
+					
+		 
+		 String househeight = JOptionPane.showInputDialog("how tall do you want the houses");
+			
+			int height = 0;
+			System.out.println(househeight);
+			if (househeight.equals("small") ) {
+				height = 60;
+				System.out.println(height);
+			}
+			
+			if (househeight.equals("medium")) {
+				height = 120;
+			}
+			
+			if (househeight.equals("large")) {
+				height = 250;
+			}
+		 
+		 
+		 
+			String red = JOptionPane.showInputDialog("color?");
+			String green = JOptionPane.showInputDialog("color?");
+			String blue = JOptionPane.showInputDialog("color?");
+			
+			int redVal = Integer.parseInt(red);
+			int greenVal = Integer.parseInt(green);
+			int blueVal = Integer.parseInt(blue);
+			
+			rob.setPenColor(redVal,greenVal,blueVal);
+			
+		 	rob.penDown();
+			
+			
 			rob.move(height);
-			rob.turn(90);
-			rob.move(30);
+			
+			if(height < 250) {
+				rob.turn(45);
+				rob.move(40);
+				rob.turn(90);
+				rob.move(40);
+				rob.setAngle(90);
+			}
+			
+			if(height >=250) {
+				rob.turn(90);
+				rob.move(30);
+			}
+			
 			rob.turn(90);
 			rob.move(height);
-		rob.setPenColor(0, 100, 10);
+	
+			rob.setPenColor(0, 100, 10);
 			rob.turn(270);
 		rob.move(50);
 			rob.turn(270);
+			 
 			rob.setPenColor(0, 0, 0);
 			}
  
